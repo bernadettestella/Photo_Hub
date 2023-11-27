724975cc12964d4012b9831fd30e7451edcc16dc
Judhunja <judxhunja@gmail.com>
2023-11-27 21:03:53 +0300

    profile creation page

--- Profile_Creation/profile.py
+++ Profile_Creation/profile.py
@@ -4,38 +4,59 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Raelynn@2017'
-app.config['MYSQL_DATABASE_DB'] = 'photo_hub'
+# MySQL configurations
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'photohub'
 
 mysql = MySQL(app)
 
 @app.route('/')
 def index():
+    cur = mysql.connection.cursor()
+
+    # Execute the MySQL query to fetch all profiles
+    cur.execute("SELECT * FROM photohub.photographers_profiles")
+
+    # Fetch all profiles
+    profiles = cur.fetchall()
+
+    # Close the cursor
+    cur.close()
+
     return render_template('index.html', profiles=profiles)
+
 
 @app.route('/create_profile', methods=['GET', 'POST'])
 def create_profile():
     if request.method == 'POST':
-        name = request.form['name']
-        age = request.form['age']
+        cur = mysql.connection.cursor()
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        username = request.form['username']
         email = request.form['email']
+        phone_number = str(request.form['phone_number'])
+        location = request.form['location']
 
-        # Create a new profile dictionary
-        new_profile = {
-            'name': name,
-            'age': age,
-            'email': email
-        }
+        # Validate input
+        if not first_name or not last_name or not username or not email or not phone_number or not location:
+            return "All fields must be filled out."
 
-        # Add the new profile to the list
-        profiles.append(new_profile)
+        # Execute the MySQL query to insert the new profile
+        cur.execute("INSERT INTO photohub.photographers_profiles (first_name, last_name, user_name, email, phone_number, location) VALUES (%s, %s, %s, %s, %s, %s)", (first_name, last_name, username, email, phone_number, location))
+
+        # Commit changes to the database
+        mysql.connection.commit()
+
+        # Close the cursor
+        cur.close()
 
         return redirect(url_for('index'))
 
     return render_template('profile.html')
 
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
