ad3f838b1d9e96554d0805241eb31f31791f435d
Kibugi1 <njugunabrian843@gmail.com>
2023-11-29 01:50:33 -0800

    Updates in the routes.py

--- hub_flask/routes.py
+++ hub_flask/routes.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 starts a Flask web application
 """
 
 from flask import Flask, render_template, request
-from flask_mysqldb import MYSQL
+from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
@@ -15,6 +15,7 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'Raelynn@2017'
 app.config['MYSQL_DATABASE_DB'] = photo_hub
 
 mysql = MYSQL(app)
+
 
 # Define a Photographer class to represent the photographers table
 class Photographer(db.Model):
@@ -29,26 +30,36 @@ class Photographer(db.Model):
     password = db.Column(db.String(100))
     email = db.Column(db.String(100))
     phone = db.Column(db.Integer)
-    
+
 
 """defining routes to various pages of the hub website"""
 # Route to handle landing page
+
+
 @app.route('/', strict_slashes=False)
 def landing_page():
+
     return render_template('landing.html')
 
 # Route to handle client welcome message
+
+
 @app.route('/client', strict_slashes=False)
 def client_login():
     return render_template('client-welcome.html')
 
-# Route to browse through photographs uploaded 
+
+# Route to browse through photographs uploaded
+
+
 @app.route('/browse_photographers', strict_slashes=False)
 def photographs_posts():
     return render_template('the page jude is using')
 
+
 # Route to handle a new photographer sign up
-@app.route('/photographers_signin', methods=['GET', 'POST'], strict_slashes=False)
+@app.route('/photographers_signin', methods=['GET', 'POST'],
+           strict_slashes=False)
 def photographer_signup():
     if request.method == 'POST':
         # Extracts the sign up form data
@@ -59,14 +70,14 @@ def photographer_signup():
         dob = request.form['dob']
         location = request.form['location']
         preferred_username = request.form['preferred_username']
-        raw_password = request.form['password']  # Get the raw password from the form
+        raw_password = request.form['password']
         confirm_password = request.form['confirm_password']
 
-	if raw_password != confirm_password:
+        if raw_password != confirm_password:
             return "Passwords do not match. Please try again."
         # Hash the raw password before storing it
         hashed_password = generate_password_hash(raw_password)
-        
+
         # Creating  a new Photographer instance with hashed password
         new_photographer = Photographer(
             first_name=first_name,
@@ -76,7 +87,7 @@ def photographer_signup():
             dob=dob,
             location=location,
             preferred_username=preferred_username,
-            password=hashed_password  # Store the hashed password in the database
+            password=hashed_password  # Store hashed password in the database
         )
 
         # Adds the new photographer to the session and commit to the database
@@ -85,15 +96,29 @@ def photographer_signup():
 
         # Redirect to login page after successful signup
         return render_template('login.html')
-    
+
     return render_template('sign_in.html')
+
 
 # Route to handle successful sign up and render login page
 @app.route('/signup_login', strict_slashes=False)
 def new_login():
-	return render_template('login.html')
+    return render_template('login.html')
+
 
 # Route to handle existing photographer login
-@app.route('/photographer_login', strict_slashes=False)
+@app.route('/photographer_login', methods=['GET', 'POST'],
+           strict_slashes=False)
 def photographer_login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Retrieve the user from the database based on the username
+        user = Photographer.query.filter_by(preferred_username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            # Successful login
+            return render_template('load_upload_images.html')
+
     return render_template('login.html')
