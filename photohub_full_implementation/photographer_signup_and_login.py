47ff21084b85c448e591c4766e35f9af56ee82aa
Judhunja <judxhunja@gmail.com>
2023-12-01 13:10:28 +0300

    full photohub implementation

--- photohub_full_implementation/photographer_signup_and_login.py
+++ photohub_full_implementation/photographer_signup_and_login.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from flask import Flask, render_template, request, redirect, url_for, session, send_file
+from flask import Flask, render_template, request, redirect, url_for, session, send_file, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import (
         LoginManager,
@@ -9,6 +9,10 @@ from flask_login import (
         logout_user,
         current_user,
         )
+from flask_wtf import FlaskForm
+from sqlalchemy.exc import IntegrityError
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 from flask_uploads import UploadSet, configure_uploads, IMAGES
@@ -33,13 +37,8 @@ login_manager.login_view = "login"
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(50))
-    surname = db.Column(db.String(50))
-    middle_name = db.Column(db.String(50))
-    gender = db.Column(db.String(10))
-    dob = db.Column(db.Date)
-    location = db.Column(db.String(100))
     preferred_username = db.Column(db.String(50), unique=True)
+    email = db.Column(db.String(256), unique=True, nullable=False)
     password = db.Column(db.String(256))
     images = db.relationship("Image", backref="user", lazy=True)
 
@@ -67,84 +66,81 @@ def landing():
     return render_template("landing.html")
 
 
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
 @app.route("/photographer_login", methods=["GET", "POST"])
 def photographer_login():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+    if current_user.is_authenticated:
+        # Redirect the user to the dashboard if they are already authenticated
+        return redirect(url_for("dashboard"))
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
 
         user = User.query.filter_by(preferred_username=username).first()
 
         if user and check_password_hash(user.password, password):
             login_user(user)
+            flash('Login successful!', 'success')
             return redirect(url_for("dashboard"))
+        else:
+            flash('Login failed. Please check your username and password.', 'error')
 
-    return render_template("login.html")
+    return render_template("login.html", form=form)
+
+
+class SignupForm(FlaskForm):
+    preferred_username = StringField('Preferred Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
 
 @app.route("/photographers_signup", methods=["GET", "POST"])
 def photographers_signup():
-    if request.method == "POST":
-        # Extract form data
-        first_name = request.form["first_name"]
-        surname = request.form["surname"]
-        middle_name = request.form["middle_name"]
-        gender = request.form["gender"]
-        dob_str = request.form["dob"]
-        # parsing dob
-        dob = datetime.strptime(dob_str, "%Y-%m-%d")
-        location = request.form["location"]
-        preferred_username = request.form["preferred_username"]
-        password = request.form["password"]
-        confirm_password = request.form["confirm_password"]
+    form = SignupForm()
 
-        # Validate password match
-        if password != confirm_password:
-            return render_template(
-                    "photographers_signup.html", error="Passwords do not match"
-                    )
+    if form.validate_on_submit():
+        preferred_username = form.preferred_username.data
+        email = form.email.data
+        password = form.password.data
+
         # Validate username uniqueness
-        existing_user = User.query.filter_by(
-                preferred_username=preferred_username
-                ).first()
+        existing_user = User.query.filter_by(preferred_username=preferred_username).first()
         if existing_user:
-            return render_template(
-                    "signup.html",
-                    error="Username is already taken. Please choose another one.",
-                    )
+            flash("Username is already taken. Please choose another one.", "error")
+            return redirect(url_for("photographers_signup"))
 
         # Hash the password
         hashed_password = generate_password_hash(password)
 
         # Insert the new user into the database
-        new_user = User(
-                first_name=first_name,
-                surname=surname,
-                middle_name=middle_name,
-                gender=gender,
-                dob=dob,
-                location=location,
-                preferred_username=preferred_username,
-                password=hashed_password,
-                )
+        new_user = User(preferred_username=preferred_username, email=email, password=hashed_password)
         db.session.add(new_user)
+
         try:
             # Attempt to commit changes to the database
             db.session.commit()
             login_user(new_user)
+            flash("Your account has been created!", "success")
             return redirect(url_for("dashboard"))
-        except IntegrityError:
-            # Handle the case where a duplicate username was inserted
+        except IntegrityError as e:
+            # Handle the case where a duplicate username or email was inserted
             db.session.rollback()
-            return render_template(
-                    "signup.html",
-                    error="Username is already taken. Please choose another one.",
-                    )
-        db.session.commit()
+            if 'unique constraint' in str(e).lower():
+                flash("Username or email is already taken. Please choose another one.", "error")
+            else:
+                flash("An unexpected error occurred. Please try again.", "error")
 
-        login_user(new_user)
-
-    return render_template("signup.html")
+    return render_template("signup.html", form=form)
 
 
 @app.route("/dashboard")
@@ -206,6 +202,54 @@ def get_image(image_id):
 def logout():
     logout_user()
     return redirect(url_for("landing"))
+
+
+@app.route("/client_welcome")
+def client_welcome():
+    return render_template("client_welcome.html")
+
+
+@app.route("/photographer_signup_login")
+def photographer_signup_login():
+    return render_template("photographer_signup_login.html")
+
+
+@app.route("/choose_category")
+def choose_category():
+    # Fetch categories
+    categories = Category.query.all()
+
+    return render_template("choose_category.html", categories=categories)
+
+
+@app.route("/browse_category_images/<int:category>")
+def browse_category_images(category):
+    photographers = User.query.filter(User.images.any(category_id=category)).all()
+    for photographer in photographers:
+        photographer.category_images = {}
+        for image_data in photographer.images:
+            if image_data.category_id == category:
+                if category not in photographer.category_images:
+                    photographer.category_images[category] = []
+                photographer.category_images[category].append(image_data)
+
+    return render_template("browse_category_images.html", category=category, photographers=photographers)
+
+
+@app.route("/photographer_profile/<int:photographer_id>")
+def photographer_profile(photographer_id):
+    photographer = User.query.get(photographer_id)
+    category_images = {}
+
+    # Fetch images for each category
+    categories = Category.query.all()
+    for category in categories:
+        category_images[category.id] = {
+                "category_name": category.name,
+                "images": Image.query.filter_by(user_id=photographer.id, category_id=category.id).all(),
+                }
+
+    return render_template("photographer_profile.html", photographer=photographer, category_images=category_images)
 
 
 if __name__ == "__main__":
